abca2895bcfb2a431bf2724c5baf98ac9766c56e
crahels <sidney.rachel@gmail.com>
2017-09-29 19:51:57 +0700

    Turn It Up Website

---
+++ TurnItUp/home.js
@@ -0,0 +1,57 @@
+var interval = 2500;
+var switching = setInterval("plusSlides(1,true)",interval);
+function plusSlides(n,isAuto) {
+  showSlides(slideIndex += n,isAuto);
+}
+
+/*function currentSlide(n) {
+  showSlides(slideIndex = n,false);
+}*/
+
+function stopTimer() {
+	clearInterval(switching);
+}
+
+function showSlides(n,isAuto) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  /*if (n == undefined) {n = ++slideIndex}*/
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  if (isAuto == false) { 
+	stopTimer();
+  	switching = setInterval("plusSlides(1,true)",interval);
+  }
+}
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}// JavaScript Document
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
